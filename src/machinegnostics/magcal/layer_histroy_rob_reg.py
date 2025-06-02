45e006b94057e490835b501ca261aac01808db1c
Nirmal Parmar <nirmalparmarphd@gmail.com>
2025-06-02 08:45:59 +0200

    history record improved

--- src/machinegnostics/magcal/layer_histroy_rob_reg.py
+++ src/machinegnostics/magcal/layer_histroy_rob_reg.py
@@ -71,49 +71,66 @@ class HistoryRobustRegressor(ParamRobustRegressorBase):
         self.data_form = data_form
         self.gnostic_characteristics = gnostic_characteristics
         self._history = history
-        self.params = []
+        self.params = [
+            {
+                'iteration': 0,
+                'loss': None,
+                'weights': None,
+                'coefficients': None,
+                'degree': self.degree,
+                'rentropy': None,
+                'fi': None,
+                'hi': None,
+                'fj': None,
+                'hj': None,
+                'infoi': None,
+                'infoj': None,
+                'pi': None,
+                'pj': None,
+                'ei': None,
+                'ej': None
+            }
+        ]
     
     def _fit(self, X: np.ndarray, y: np.ndarray):
         """
         Fit the model to the data and record history.
-        
+
         Parameters
         ----------
         X : np.ndarray
             Input features.
         y : np.ndarray
             Target values.
-        """       
+        """
         # Call the parent fit method to perform fitting
         super()._fit(X, y)
-        
-        # Record the initial state in history
+
+        # Record the initial state in history as a dict
+        params_dict = {}
+
         if self.gnostic_characteristics:
-            initial_record = ParamRecord(
-                iteration=self._iter + 1,
-                h_loss=self.loss,
-                weights=self.weights.copy(),
-                coefficients=self.coefficients.copy(),
-                degree=self.degree,
-                rentropy=self.re,
-                fi= self.fi,
-                hi= self.hi,
-                fj= self.fj,
-                hj= self.hj,
-                infoi= self.infoi,
-                infoj= self.infoj,
-                pi= self.pi,
-                pj= self.pj,
-                ei= self.ei,
-                ej= self.ej
-            )
-            self.params.append(initial_record)
+            params_dict['iteration'] = self._iter + 1
+            params_dict['loss'] = self.loss
+            params_dict['weights'] = self.weights.copy() if self.weights is not None else None
+            params_dict['coefficients'] = self.coefficients.copy() if self.coefficients is not None else None
+            params_dict['degree'] = self.degree
+            params_dict['rentropy'] = self.re
+            params_dict['fi'] = self.fi
+            params_dict['hi'] = self.hi
+            params_dict['fj'] = self.fj
+            params_dict['hj'] = self.hj
+            params_dict['infoi'] = self.infoi
+            params_dict['infoj'] = self.infoj
+            params_dict['pi'] = self.pi
+            params_dict['pj'] = self.pj
+            params_dict['ei'] = self.ei
+            params_dict['ej'] = self.ej
         else:
-            initial_record = ParamRecord(
-                iteration=0,
-                h_loss=None,
-                weights=self.weights,
-                coefficients=self.coefficients,
-                degree=self.degree
-            )
-            self.params.append(initial_record)
+            params_dict['iteration'] = 0
+            params_dict['loss'] = None
+            params_dict['weights'] = self.weights.copy() if self.weights is not None else None
+            params_dict['coefficients'] = self.coefficients .copy() if self.coefficients is not None else None
+            params_dict['degree'] = self.degree
+
+        self.params.append(params_dict)
