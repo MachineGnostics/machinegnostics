15990a1e352d23d937fb08864dc83d98bc6e91a1
Nirmal Parmar <nirmalparmarphd@gmail.com>
2025-09-08 18:11:26 +0200

    qgdf public class basic added - tested

---
+++ src/machinegnostics/magcal/gdf/qgdf.py
@@ -0,0 +1,63 @@
+'''
+QGDF Quantifying Global Distribution Function (QGDF)
+
+Public class
+
+Author: Nirmal Parmar
+Machine Gnostics
+'''
+
+import numpy as np
+from machinegnostics.magcal.gdf.base_qgdf import BaseQGDF
+
+class QGDF(BaseQGDF):
+    def __init__(self,
+                 data: np.ndarray,
+                 DLB: float = None,
+                 DUB: float = None,
+                 LB: float = None,
+                 UB: float = None,
+                 S = 'auto',
+                 z0_optimize: bool = True,
+                 tolerance: float = 1e-3,
+                 data_form: str = 'a',
+                 n_points: int = 500,
+                 homogeneous: bool = True,
+                 catch: bool = True,
+                 weights: np.ndarray = None,
+                 wedf: bool = True,
+                 opt_method: str = 'L-BFGS-B',
+                 verbose: bool = False,
+                 max_data_size: int = 1000,
+                 flush: bool = True):
+        """Initialize QGDF with parameters and verbosity."""
+        super().__init__(data=data,
+                         DLB=DLB,
+                         DUB=DUB,
+                         LB=LB,
+                         UB=UB,
+                         S=S,
+                         z0_optimize=z0_optimize,
+                         tolerance=tolerance,
+                         data_form=data_form,
+                         n_points=n_points,
+                         homogeneous=homogeneous,
+                         catch=catch,
+                         weights=weights,
+                         wedf=wedf,
+                         opt_method=opt_method,
+                         verbose=verbose,
+                         max_data_size=max_data_size,
+                         flush=flush)
+
+    def fit(self, plot: bool = False):
+        """Public method to fit QGDF and optionally plot results."""
+        self._fit_qgdf(plot=plot)
+
+    def plot(self, plot_smooth: bool = True, plot: str = 'both', bounds: bool = True, extra_df: bool = True, figsize: tuple = (12, 8)):
+        """Public method to plot QGDF and/or PDF results."""
+        self._plot(plot_smooth=plot_smooth, plot=plot, bounds=bounds, extra_df=extra_df, figsize=figsize)
+    
+    def results(self) -> dict:
+        """Public method to get fitting results."""
+        return self._get_results()
