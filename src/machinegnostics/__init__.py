9d8acfa8cde734207db84cf6994b1ec6b716785f
Nirmal Parmar <nirmalparmarphd@gmail.com>
2025-09-19 22:11:43 +0200

    imports updated -tested

--- src/machinegnostics/__init__.py
+++ src/machinegnostics/__init__.py
@@ -1,4 +1,7 @@
 # quick methods
 from .metrics.mean import mean
+from .metrics.median import median
 from .metrics.variance import variance
 from .metrics.std import std
+from .metrics.gcorrelation import gcorrelation
+from .magcal.gcov import gcovariance
