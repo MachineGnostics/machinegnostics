f8b66c2bdcd956e4da7dcac86e2e00136b38afa4
Nirmal Parmar <nirmalparmarphd@gmail.com>
2025-09-23 10:39:39 +0200

    imports updated, removed gcov

--- src/machinegnostics/__init__.py
+++ src/machinegnostics/__init__.py
@@ -4,4 +4,3 @@ from .metrics.median import median
 from .metrics.variance import variance
 from .metrics.std import std
 from .metrics.gcorrelation import gcorrelation
-from .magcal.gcov import gcovariance
