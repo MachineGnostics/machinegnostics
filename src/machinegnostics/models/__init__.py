b9bebaf6b385d41a77174dfb0073473a23aa5b82
Nirmal Parmar <nirmalparmarphd@gmail.com>
2026-01-13 13:14:53 +0100

    lin-reg added

--- src/machinegnostics/models/__init__.py
+++ src/machinegnostics/models/__init__.py
@@ -1,2 +1,6 @@
+# models
+from machinegnostics.models.regression.linear_regressor import LinearRegressor
+
+# support
 from machinegnostics.models.cross_validation import CrossValidator
 from machinegnostics.models.data_split import train_test_split
